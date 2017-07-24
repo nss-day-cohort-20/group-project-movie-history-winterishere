e966eacfab49f96a3ca880b0f6ee6f1e3b655094
Jules Sepulveda <julietrsepulveda@gmail.com>
2017-07-24 09:17:01 -0500

    new movies enter clearand populate dom with new movies

--- javascripts/main.js
+++ javascripts/main.js
@@ -11,16 +11,22 @@ controller.addToWatchList();
 //When the user clicks the log in link, this calls the function to log them in with firebase
 //It also loads the user's movies to the DOM
 $("#log-on").click( function() {
+	$("#log-out").addClass("hideIt");
+	$(".messagePreLogin").addClass("hideIt");
 	userFactory.logInGoogle()
 	.then( (result) => {
 		let user = result.user.uid;
 		console.log("user", user);
+		$("#log-out").removeClass("hideIt");
+		$(".messagePostLogin").removeClass("hideIt");
+
 	});
 });
 
 //When the user clicks the log out link, this calls the function to log them out
 //it also refreshes the page so that the user returns to the default website
 $("#log-out").click( function() {
+	$("#log-out").addClass("hideIt");
 	userFactory.logOutGoogle();
 	location.reload();
 });
