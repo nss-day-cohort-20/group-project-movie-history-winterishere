f8b7117d200259ebb9d7b02b61f026ab2f0cc485
Jules Sepulveda <julietrsepulveda@gmail.com>
2017-07-24 11:08:29 -0500

    added stars when add to watchlist clicked

--- javascripts/main.js
+++ javascripts/main.js
@@ -12,6 +12,7 @@ $("#log-on").click( function() {
 	$(".messagePreLogin").addClass("hideIt");
 	userFactory.logInGoogle()
 	.then( (result) => {
+		$("#search-bar").prop("disabled", false);
 		let user = result.user.uid;
 		console.log("user", user);
 		$("#log-out").removeClass("hideIt");
@@ -30,7 +31,18 @@ $("#log-out").click( function() {
 
 $(document).on("click", ".card-link", function() {
 	let movieId = $(this).data("add-watch");
+	$(`#${movieId}-add-watchlist`).addClass("hideIt");
+	$(`#${movieId}-stars-container`).removeClass("hideIt");
 	controller.addToWatchList(movieId);
+});
+
+$(document).on("click", ".star", function() {
+	let thisStarIndex = $(this).attr("id").split("-");
+	let starMovieId = thisStarIndex[0];
+	console.log(thisStarIndex);
+	for (let i = 1; i <= thisStarIndex[2]; i++) {
+		document.getElementById(`${thisStarIndex[0]}-star-${i}`).classList.add("rated");
+	}
 });
 
 $(document).keypress(function(e) {
